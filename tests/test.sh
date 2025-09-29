2b80dad2e41f7ade1358bf07a3703f3de0af503d
Gustavo Schoenaker <ranaur2010@ranaur.net>
2025-09-29 19:19:47 -0300

    general evolution, but this test still sucks

--- tests/test.sh
+++ tests/test.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 function fc() {
-	`dirname $0`/filecheck $*
+	`dirname $0`/../filecheck.sh $*
 }
 
-function assertMD5() {
-	if [ $2. == . ] ; then
-		echo "*** FILE: $1"
-		cat $1
-		read -p "MD5 = "`md5sum $1 | cut -b 1-32`" Continue? "
-	else
-		if [ `md5sum $1 | cut -b 1-32` != $2 ] ; then
-			>&2 echo "ERROR: file $1 has wrong MD5 (not $2)"
-		fi
+function assertFileMD5() {
+    if [ ! -f "$1" ] ; then
+		>&2 echo "ERROR: file $1 does not exist"
+        exit 1
+    fi
+
+	if [ `md5sum $1 | cut -b 1-32` != "$2" ] ; then
+		>&2 echo "ERROR: file $1 has wrong MD5 (not $2)"
+	fi
+}
+
+function assertFilecheckMD5() {
+    if [ ! -f "$1" ] ; then
+		>&2 echo "ERROR: file $1 does not exist"
+        exit 1
+    fi
+
+	FILECHECK="$(dirname "$1")/.filecheck"
+    if [ ! -f "$FILECHECK" ] ; then
+		>&2 echo "ERROR: file $FILECHECK does not exist"
+        exit 1
+    fi
+
+	FILECHECK_MD5=$(grep ":$(basename "$1")" "$FILECHECK" | cut -d : -f 1)
+	FILE_MD5=$(md5sum "$1" | cut -b 1-32)
+
+	if [ "$FILECHECK_MD5" != "$FILE_MD5" ] ; then
+		>&2 echo "ERROR: file $1 has wrong MD5 ($FILE_MD5 and not $FILECHECK_MD5)"
 	fi
 }
 
@@ -22,7 +41,7 @@ mkdir test
 fc generate test
 fc check test
 
-assertMD5 test/.filecheck edb8fe59d0410d8a2f51af16d4ed551e
+assertFileMD5 test/.filecheck edb8fe59d0410d8a2f51af16d4ed551e
 
 echo "One" > test/One
 
@@ -30,8 +49,7 @@ echo "One" > test/One
 fc generate test 
 fc check test
 
-ONE=`md5sum test/One | cut -b 1-32`
-assertMD5 test/.filecheck $ONE
+assertFilecheckMD5 test/One
 
 	# subdir without -r
 mkdir test/sub
@@ -40,13 +58,8 @@ echo "Sub" > test/sub/Two
 fc generate test
 fc check test
 
-assertMD5 test/.filecheck $ONE
- 
 fc generate -r test
 fc check -r test
-
-assertMD5 test/.filecheck
-assertMD5 test/sub/.filecheck
 
 touch test/sub/Two
 touch test/sub/Three
@@ -54,7 +67,7 @@ touch test/sub/Three
 fc check -r test
 fc update -r test
 
-assertMD5 test/sub/.filecheck
+#assertMD5 test/sub/.filecheck
 
 fc check -r test
 
